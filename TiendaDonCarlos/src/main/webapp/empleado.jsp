9842d15d9aea8d190e599a068435b601a1db788d
Mars0606 <mars3507921@gmail.com>
2025-07-07 16:31:44 -0500

    dashboard registro cliente

---
+++ TiendaDonCarlos/src/main/webapp/empleado.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : empleado
+    Created on : 7/07/2025, 4:24:25 p. m.
+    Author     : MARS
+--%>
+
+<%@page contentType="text/html" pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>JSP Page</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
---
+++ TiendaDonCarlos/target/generated-sources/annotations/logica/Rol_.java
@@ -0,0 +1,14 @@
+package logica;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.7.12.v20230209-rNA", date="2025-07-07T15:57:21")
+@StaticMetamodel(Rol.class)
+public class Rol_ { 
+
+    public static volatile SingularAttribute<Rol, Integer> id;
+    public static volatile SingularAttribute<Rol, String> nombre;
+
+}
